ed8059c6a1c01f1fe709af258230fc29ef630de9
a20adrlopcar <a20adrlopcar@nspedralbes.cat>
2022-04-29 11:50:24 +0200

    Testing hecho junit

---
+++ src/main/java/com/example/ekonos_logica/Main/MainTest.java
@@ -0,0 +1,17 @@
+package com.example.ekonos_logica.Main;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void comprovar() {
+
+
+        var testBd= new Tauler();
+        assertEquals(2, testBd.taulerEuropeu());
+    }
+}
